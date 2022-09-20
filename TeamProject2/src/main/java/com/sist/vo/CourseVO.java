e0467978950e81dd63622ed6dd32ff5edbf94803
ajskd5 <ajskd5@naver.com>
2022-09-20 17:44:16 +0900

    이종환 - 코스짜기

---
+++ TeamProject2/src/main/java/com/sist/vo/CourseVO.java
@@ -0,0 +1,12 @@
+package com.sist.vo;
+import java.util.*;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class CourseVO {
+	private int no, inwon;
+	private String rday, rtime, place, addr, dbday;
+	private Date regdate;
+}
