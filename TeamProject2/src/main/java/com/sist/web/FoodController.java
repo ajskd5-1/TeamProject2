f871689eba0e65b85552ff1299fa1085749df1ca
jihyunss <ji_hyun_98@naver.com>
2022-09-20 17:33:25 +0900

    서지현 - 관광지 리스트 출력

--- TeamProject2/src/main/java/com/sist/web/FoodController.java
+++
@@ -1,8 +0,0 @@
-package com.sist.web;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class FoodController {
-}
